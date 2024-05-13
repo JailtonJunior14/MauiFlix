4f6950ce3a4964f6eefe54e4649698d66b2daf14
JailtonJUnior14 <jailtonjrbr@gmail.com>
2024-05-13 15:56:34 -0300

    Tela principal finalizada.

--- MauiFlix/MainPage.xaml.cs
+++ MauiFlix/MainPage.xaml.cs
@@ -2,6 +2,12 @@
 {
     public partial class MainPage : ContentPage
     {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
         private void animacao_Clicked(System.Object sender, System.EventArgs e)
         {
 
@@ -13,6 +19,51 @@
         }
 
         private void aventura_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void comedia_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void documentario_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void drama_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ficcao_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guerra_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void infantil_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nacional_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void romance_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void suspense_Clicked(object sender, EventArgs e)
         {
 
         }
